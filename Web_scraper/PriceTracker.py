c816017d8db614c807cf15e881b85d1418fa8311
Heecheon <heecheon92@gmail.com>
2019-07-21 01:47:54 -0500

    Price tracker added.

---
+++ Web_scraper/PriceTracker.py
@@ -0,0 +1,71 @@
+"""
+Name: Heecheon Park
+Description: Please look at the README
+"""
+
+import requests
+import string
+import re
+import smtplib
+from bs4 import BeautifulSoup
+
+URL = 'https://www.amazon.com/Evlution-Pre-workout-Pre-Workout-Watermelon-Pikatropin-Free/dp/B01EE5DCT2/ref=sr_1_2_sspa?crid=4UX4N7TQLX3T&keywords=engine%2Bshred%2Bpre%2Bworkout&qid=1563679668&s=gateway&sprefix=engine%2Bshred%2Caps%2C171&sr=8-2-spons&smid=A2PLYW0DW8DU2C&th=1'
+
+headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15'}
+
+def check_price():
+        
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'lxml')
+
+# Inefficient way..... found the better solution.
+#title = str(soup.find(id="title_feature_div").find_all("span")).strip()
+#title = re.split(r'\s{2,}', title)[1]
+
+    """
+    Beautiful Soup find() may not find a tag or element,
+    because some websites run JS DOM manipulation.
+    A tag that I am looking for might not show up if I turn off the javascript.
+    If find() cannot find a tag, try to look for parent tag and then find a child tag.
+    Like below.
+    """
+    title = soup.find(id="title_feature_div").find("span", id="productTitle").get_text()
+    print("Product: "+title.strip())
+
+    price = soup.find(id="priceblock_snsprice_Based").get_text()
+# First time that strip could not remove white space.
+    price = re.split(r"^\s+|\s+$", price)[1].strip()
+# Additional measure to remove white space.
+    """
+    From the documentation of the string.whitespace:
+    A string containing all ASCII characters that are considered whitespace. This includes the characters space, tab, linefeed, return, formfeed, and vertical tab.
+    """
+    price = price.translate({ord(c): None for c in string.whitespace})
+    converted_price = float(price[1:6])
+    print("Current Price: $"+str(converted_price))
+
+    if (converted_price < 28.00):
+        send_mail()
+
+def send_mail():
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login("your_username@gmail.com", "your password")
+    subject = "Price fell down!"
+    body = "Check the Amazon link https://www.amazon.com/Evlution-Pre-workout-Pre-Workout-Watermelon-Pikatropin-Free/dp/B01EE5DCT2/ref=sr_1_2_sspa?crid=4UX4N7TQLX3T&keywords=engine%2Bshred%2Bpre%2Bworkout&qid=1563679668&s=gateway&sprefix=engine%2Bshred%2Caps%2C171&sr=8-2-spons&smid=A2PLYW0DW8DU2C&th=1"
+
+    msg = f"Subject: {subject}\n\n{body}"
+    server.sendmail(
+            "your_send_username@gmail.com",
+            "your_receive_username@anothermail.com",
+            msg
+    )
+
+    print("Notification mail has been sent!")
+    server.quit()
+
+if __name__ == "__main__":
+    check_price()
---
+++ Web_scraper/README.md
@@ -0,0 +1,7 @@
+This is a price tracker.
+
+I am still building for anyone can use but for now, I am using this as a personal script.
+
+For now, this program will check a product on a webpage (Amazon in this case) and track the price of an item.
+
+If the price goes down, google mail server will send a notification mail that the price has gone down.
